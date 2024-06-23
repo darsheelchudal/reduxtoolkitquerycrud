e72d076a5e65930ca87c5a22cdadb3c086ee6c53
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-23 07:52:46 +0545

    model and task slice

--- src/features/taskSlice.tsx
+++ src/features/taskSlice.tsx
@@ -1,12 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { Task } from "../model/tasks.model";
 
-const TaskApi = createApi({
+export const TaskApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://66777c1a145714a1bd74d7b3.mockapi.io/api/v1",
   }),
   endpoints: (builder) => ({
-    tasks: builder.query<Tasks[], void>({
-      query: () => "/posts",
+    tasks: builder.query<Task[], void>({
+      query: () => "/tasks",
     }),
   }),
 });
---
+++ src/model/tasks.model.tsx
@@ -0,0 +1,5 @@
+export interface Task {
+  id: String;
+  taskName: String;
+  taskType: String;
+}
